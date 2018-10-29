5ea23ba3d9ce6bf1a1fafbc1c070d33137a62cb2
Jared Rummler <jared.rummler@gmail.com>
2018-10-28 17:45:07 -0700

    Migrate to AndroidX

--- demo/src/main/java/com/jaredrummler/materialspinner/example/MainActivity.java
+++ demo/src/main/java/com/jaredrummler/materialspinner/example/MainActivity.java
@@ -21,10 +21,10 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import com.jaredrummler.materialspinner.MaterialSpinner;
 
