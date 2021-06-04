a273d21c0923bc2b3e0652a5269d7666a9e4904b
Scott Alexander-Bown <scottyab@users.noreply.github.com>
2021-06-04 15:05:59 +0100

    Add demo example of spinner with custom adapter and the fix to getItemText is working

--- demo/src/main/java/com/jaredrummler/materialspinner/example/MainActivity.java
+++ demo/src/main/java/com/jaredrummler/materialspinner/example/MainActivity.java
@@ -18,6 +18,7 @@
 package com.jaredrummler.materialspinner.example;
 
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -27,56 +28,119 @@ import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.jaredrummler.materialspinner.MaterialSpinner;
+import com.jaredrummler.materialspinner.MaterialSpinnerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-  private static final String[] ANDROID_VERSIONS = {
-      "Cupcake",
-      "Donut",
-      "Eclair",
-      "Froyo",
-      "Gingerbread",
-      "Honeycomb",
-      "Ice Cream Sandwich",
-      "Jelly Bean",
-      "KitKat",
-      "Lollipop",
-      "Marshmallow",
-      "Nougat",
-      "Oreo"
-  };
+    private static final String[] ANDROID_VERSIONS = {
+            "Cupcake",
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow",
+            "Nougat",
+            "Oreo"
+    };
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/jaredrummler/MaterialSpinner")));
+                } catch (ActivityNotFoundException ignored) {
+                }
+            }
+        });
 
-      @Override public void onClick(View view) {
-        try {
-          startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/jaredrummler/MaterialSpinner")));
-        } catch (ActivityNotFoundException ignored) {
+        setupSpinner1();
+        setupSpinner2();
+    }
+
+    private void setupSpinner1() {
+        MaterialSpinner spinner = (MaterialSpinner) findViewById(R.id.spinner);
+        spinner.setItems(ANDROID_VERSIONS);
+        spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+            @Override
+            public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_SHORT).show();
+            }
+        });
+        spinner.setOnNothingSelectedListener(new MaterialSpinner.OnNothingSelectedListener() {
+
+            @Override
+            public void onNothingSelected(MaterialSpinner spinner) {
+                Snackbar.make(spinner, "Nothing selected", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void setupSpinner2() {
+        MaterialSpinner spinner2 = (MaterialSpinner) findViewById(R.id.spinner2);
+
+        List cheeses = new ArrayList();
+        cheeses.add(new Cheese("Edam", "Ne"));
+        cheeses.add(new Cheese("Cheddar", "Uk"));
+        cheeses.add(new Cheese("Brie", "Fr"));
+
+        MyCustomCheeseAdapter adapter = new MyCustomCheeseAdapter(this, cheeses);
+        spinner2.setAdapter(adapter);
+        spinner2.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<Cheese>() {
+
+            @Override
+            public void onItemSelected(MaterialSpinner view, int position, long id, Cheese item) {
+                Snackbar.make(view, "Cheese clicked " + item, Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    class Cheese {
+        String name;
+        String origin;
+
+        public Cheese(String name, String origin) {
+            this.name = name;
+            this.origin = origin;
         }
-      }
-    });
 
-    MaterialSpinner spinner = (MaterialSpinner) findViewById(R.id.spinner);
-    spinner.setItems(ANDROID_VERSIONS);
-    spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("Cheese{");
+            sb.append("name='").append(name).append('\'');
+            sb.append(", origin='").append(origin).append('\'');
+            sb.append('}');
+            return sb.toString();
+        }
+    }
 
-      @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
-        Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_LONG).show();
-      }
-    });
-    spinner.setOnNothingSelectedListener(new MaterialSpinner.OnNothingSelectedListener() {
+    class MyCustomCheeseAdapter extends MaterialSpinnerAdapter<Cheese> {
 
-      @Override public void onNothingSelected(MaterialSpinner spinner) {
-        Snackbar.make(spinner, "Nothing selected", Snackbar.LENGTH_LONG).show();
-      }
-    });
-  }
+        public MyCustomCheeseAdapter(Context context, List<Cheese> items) {
+            super(context, items);
+        }
+
+        @Override
+        public String getItemText(int position) {
+            return super.getItem(position).name;
+        }
+    }
 
 }
